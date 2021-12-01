4ff4ac04550e416b91b084ef83819c1ee83eccb1
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-01 20:19:53 +0800

    Delete lib directory

--- lib/jsimple/railtie.rb
+++
@@ -1,9 +0,0 @@
-require 'jsimple/rails_view_helper'
-
-module Jsimple
-  class JsimpleRailtie < ::Rails::Railtie
-    initializer 'jsimple.view_helpers' do
-      ActionView::Base.send :include, RailsViewHelper
-    end
-  end
-end
--- lib/jsimple/version.rb
+++
@@ -1,3 +0,0 @@
-module Jsimple
-  VERSION = '0.5.0'
-end
